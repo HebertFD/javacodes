a086efa31027d64839818183d3b664e9680dfb78
HebertFD <hebertfd33@gmail.com>
2024-09-11 18:04:37 -0300

    imc

---
+++ Imc/src/imc/Imc.java
@@ -0,0 +1,12 @@
+
+package imc;
+
+public class Imc {
+
+    public static void main(String[] args) {
+        GUI_Imc gui_Imc = new GUI_Imc();
+        gui_Imc.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
